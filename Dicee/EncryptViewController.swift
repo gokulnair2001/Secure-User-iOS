5f0fd2b090b8497ac36e694085c1b161f9bdd260
pateldevang <devangpatel92@gmail.com>
2020-02-19 23:25:16 +0530

    New

--- Dicee/EncryptViewController.swift
+++ Dicee/EncryptViewController.swift
@@ -7,24 +7,57 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
-class EncryptViewController: UIViewController {
+class EncryptViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var pickedimage: UIImageView!
+    
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+//
+//            pickedimage.image = userPickedImage
+//
+//            guard let ciimage = CIImage(image: userPickedImage) else {
+//                fatalError("Could not convert to Ci image")
+//            }
+//
+//        }
+//
+//        imagePicker.dismiss(animated: true, completion: nil)
+//
+//    }
+    
+    
+    private func imagePickerController(_ picker: UIImagePickerController,
+                                       didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            return
+        }
+        pickedimage.image = image
     }
-    */
+    
+    @IBAction func imagepick(_ sender: Any) {
+        
+        
+         present(imagePicker, animated: true, completion: nil)
+
+        
+    }
+    
 
 }
