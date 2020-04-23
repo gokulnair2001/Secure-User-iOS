f15672ca55a9d86941c826984178852442a257d0
piyushxbajaj <piyushpiyush41@gmail.com>
2020-04-23 17:53:41 +0530

    Image picker done

--- Dicee/EncryptViewController.swift
+++ Dicee/EncryptViewController.swift
@@ -11,53 +11,107 @@ import Vision
 import CoreML
 
 class EncryptViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-
-    @IBOutlet weak var pickedimage: UIImageView!
-    
-    
     let imagePicker = UIImagePickerController()
+
+    
+    
+    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.allowsEditing = false
+            super.viewDidLoad()
     }
     
     
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//
-//        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-//
-//            pickedimage.image = userPickedImage
-//
-//            guard let ciimage = CIImage(image: userPickedImage) else {
-//                fatalError("Could not convert to Ci image")
-//            }
-//
-//        }
-//
-//        imagePicker.dismiss(animated: true, completion: nil)
-//
+    @IBOutlet weak var pickedimage: UIImageView!
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        pickedimage.layer.cornerRadius = imageView.frame.size.width/2
+//        pickedimage.layer.masksToBounds = true
 //    }
     
     
-    private func imagePickerController(_ picker: UIImagePickerController,
-                                       didFinishPickingMediaWithInfo info: [String : Any]) {
-        picker.dismiss(animated: true, completion: nil)
-        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+    
+    
+    @IBAction func openLibraryAction(_ sender: UIButton) {
+        openPhotoLibrary()
+    }
+    func openPhotoLibrary(){
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary)else{
+            print("Photo library cannot be opened")
             return
         }
-        pickedimage.image = image
-    }
-    
-    @IBAction func imagepick(_ sender: Any) {
-        
-        
-         present(imagePicker, animated: true, completion: nil)
-
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker,animated: true)
         
     }
     
-
 }
+
+//MARK:- imagePICKER
+extension EncryptViewController{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    defer {
+        picker.dismiss(animated: true)
+    }
+    
+    print(info)
+    // get the image
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+        return
+    }
+        pickedimage.image = image
+    // do something with it
+    
+        
+    }
+}
+    
+//    let imagePicker = UIImagePickerController()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        imagePicker.delegate = self
+//        imagePicker.sourceType = .photoLibrary
+//        imagePicker.allowsEditing = false
+//    }
+//
+//
+////    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+////
+////        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+////
+////            pickedimage.image = userPickedImage
+////
+////            guard let ciimage = CIImage(image: userPickedImage) else {
+////                fatalError("Could not convert to Ci image")
+////            }
+////
+////        }
+////
+////        imagePicker.dismiss(animated: true, completion: nil)
+////
+////    }
+//
+//
+//    private func imagePickerController(_ picker: UIImagePickerController,
+//                                       didFinishPickingMediaWithInfo info: [String : Any]) {
+//        picker.dismiss(animated: true, completion: nil)
+//        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+//            return
+//        }
+//        pickedimage.image = image
+//    }
+//
+//
+//    @IBAction func imagepick(_ sender: Any) {
+//
+//
+//         present(imagePicker, animated: true, completion: nil)
+//
+//
+//
+//    }
+//
+//
+//}
